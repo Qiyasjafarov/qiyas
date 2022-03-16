ebdedc38b6fd00565b723666a1181abe9a49e810
Qiyas <7f2ng5y@code.edu.az>
2022-03-17 01:28:24 +0400

    03.16 h

---
+++ 03.16 hometask/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Sas
+{
+static void Main()
+{
+
+    Console.WriteLine(CalculateArea(8,40));
+        
+}
+
+    static int CalculateArea(int r)
+    {if (r > 0)
+        {
+            int s = 3 * r * r;
+
+            return s;
+        }
+        else return -1;
+
+    }
+    static int CalculateArea(int a, int b)
+    {
+        if (a > 0 && b > 0)
+        {
+            int s = a * b;
+
+            return s;
+        }
+        else return -1;
+    }
+
+    static int CalculateArea(int a, int b, int c)
+    {
+        if (a > 0 && b > 0 && c > 0)
+        {
+            int s = 2 * (a * b + a * c + b * c);
+
+            return s;
+        }
+        else return -1;
+    }
+}
